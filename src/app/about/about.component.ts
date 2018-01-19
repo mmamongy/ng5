a7396fe3c24b7600d99e05493ff25e77792c2ce9
mmamongy <mariummamdouh@gmail.com>
2018-01-19 22:31:15 +0200

    Adding Routing

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute , Router } from '@angular/router' ;
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router , private route : ActivatedRoute) {
+    this.route.params.subscribe(res => {
+      console.log(res.id) ;
+    });
+   }
 
   ngOnInit() {
+  }
+  sendMeHome() {
+    this.router.navigate(['']);
   }
 
 }
