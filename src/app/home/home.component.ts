4a445b5556d647f21033f70081628cf376d2887e
mmamongy <mariummamdouh@gmail.com>
2018-01-19 20:48:30 +0200

    Adding animation library

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import {trigger , stagger, query , animate , keyframes, style , transition} from '@angular/animations';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('goals',[
+      transition('* => *',[
+        query(':enter',style({
+          opacity: 0
+        }),{optional:true}),
+
+        query(':enter',stagger('300ms',[
+          animate('.6s ease-in',keyframes([ 
+            style({opacity:0, transform: 'translateY(-75%)',offset:0}),
+            style({opacity:.5, transform: 'translateY(35px)',offset:.3}),
+            style({opacity:1, transform: 'translateY(0)',offset:1})
+          ]))]),{optional:true}),
+
+          query(':leave',stagger('300ms',[
+          animate('.6s ease-in',keyframes([ 
+            style({opacity:1, transform: 'translateY(0)',offset:0}),
+            style({opacity:.5, transform: 'translateY(35px)',offset:.3}),
+            style({opacity:0, transform: 'translateY(-75%)',offset:1})
+          ]))]),{optional:true})
+      ])
+
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
   itemsCount = 0 ;
-  goals = [] ;
+  goals = ["x" , "Y" , "z"] ;
   btnText = 'Add an item' ;
   goalText = 'what is your life goal';
   constructor() { }
@@ -20,6 +44,10 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText) ;
     this.itemsCount = this.goals.length;
     this.goalText = '';
+  }
+
+  removeItem(i){
+    this.goals.splice(i,1);
   }
 
 }
